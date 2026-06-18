212d49dc37673d89dbed137b82469ad2bc8ee2db
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-18 12:18:32 +0530

    refactor(EmployeeWage): UC6 Calculate Wage Till Max Working Hours or Days

--- Employee_Wage_Computation_Problem/EmployeeWage.java
+++ Employee_Wage_Computation_Problem/EmployeeWage.java
@@ -5,15 +5,23 @@ public class EmployeeWage {
 	static final int WAGE_PER_HOUR = 20;
 	static final int PART_TIME_HOURS = 4;
 	static final int FULL_TIME_HOURS = 8;
-	static final int WORKING_DAYS = 20;
+
+	static final int MAX_WORKING_DAYS = 20;
+	static final int MAX_WORKING_HOURS = 100;
+
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
+		int totalHours = 0;
+		int totalDays = 0;
 		int totalWage = 0;
 
-		for (int day = 1; day <= WORKING_DAYS; day++) {
+		while (totalHours < MAX_WORKING_HOURS
+				&& totalDays < MAX_WORKING_DAYS) {
+
+			totalDays++;
 
 			int empCheck = (int) (Math.random() * 3);
 
@@ -33,9 +41,12 @@ public class EmployeeWage {
 				workHours = 0;
 			}
 
+			totalHours += workHours;
 			totalWage += workHours * WAGE_PER_HOUR;
 		}
 
+		System.out.println("Total Days = " + totalDays);
+		System.out.println("Total Hours = " + totalHours);
 		System.out.println("Monthly Wage = " + totalWage);
 	}
 

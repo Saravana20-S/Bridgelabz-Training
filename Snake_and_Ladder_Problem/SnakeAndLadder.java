f6cca61c4dc8291912f96d99243518161f9bc6b2
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-18 15:41:44 +0530

    refactor(SnakeAndLadder): Implement die roll functionality

--- Snake_and_Ladder_Problem/SnakeAndLadder.java
+++ Snake_and_Ladder_Problem/SnakeAndLadder.java
@@ -1,5 +1,16 @@
 package com.bridgelabz.Snake_and_Ladder_Problem;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
+	
+	public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int dieRoll = random.nextInt(6) + 1;
+
+        System.out.println("Die Number: " + dieRoll);
+    }
 
 }

5dce41cde10cb7eb76df0f96f44c4b6138a3d95c
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-18 16:43:38 +0530

    feat(logicalProblems): Reverse Number program

---
+++ logicalproblems/ReverseNumber.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.logicalproblems;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int reverse = 0;
+
+        while (number != 0) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+
+        System.out.println("Reversed Number: " + reverse);
+
+        sc.close();
+    }
+
+}

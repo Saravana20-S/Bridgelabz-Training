a75871c63bcd3cac3e6c23fc0cdb9f6dd95c906f
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-18 15:30:24 +0530

    refactor(LineComparison): UC1 Calculate length of a line using Cartesian coordinates

--- Line_Comparison_Problem/LineComparison.java
+++ Line_Comparison_Problem/LineComparison.java
@@ -1,23 +1,32 @@
 package com.bridgelabz.Line_Comparison_Problem;
 
+import java.util.Scanner;
+
 public class LineComparison {
+	public static double calculateLength(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) +
+                         Math.pow(y2 - y1, 2));
+    }
 
-	static String s;
+    public static void main(String[] args) {
 
-	public static void printValue(Object obj) {
-		System.out.println("Object version called");
-	}
+        Scanner sc = new Scanner(System.in);
 
-	public static void printValue(String str) {
-		System.out.println("String version called");
-	}
+        System.out.println("Enter x1 y1 x2 y2:");
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        double length = calculateLength(x1, y1, x2, y2);
+
+        System.out.println("Length of Line = " + length);
+
+        sc.close();
+    }
+
 	
-	
-	public static void main(String[] args) {
-		System.out.println(s);
-//			printValue(null);
-	}
-
 }
 
 

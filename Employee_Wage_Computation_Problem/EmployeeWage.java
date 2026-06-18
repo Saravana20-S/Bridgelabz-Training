120918a9aa3bc7b4db777062f4210c9d7578a66d
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-18 12:12:14 +0530

    refactor(EmployeeWage): UC3 Add Part Time Employee and Wage

--- Employee_Wage_Computation_Problem/EmployeeWage.java
+++ Employee_Wage_Computation_Problem/EmployeeWage.java
@@ -3,26 +3,34 @@ package com.bridgelabz.Employee_Wage_Computation_Problem;
 public class EmployeeWage {
 
 	static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOURS = 8;
-	
+	static final int PART_TIME_HOURS = 4;
+	static final int FULL_TIME_HOURS = 8;
+
 	public static void main(String[] args) {
-		 
-		 System.out.println("Welcome to Employee Wage Computation Program");
 
-	        int attendance = (int) (Math.random() * 2);
+		System.out.println("Welcome to Employee Wage Computation Program");
 
-	        if (attendance == 1) {
+		int empCheck = (int) (Math.random() * 3);
 
-	            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+		if (empCheck == 1) {
 
-	            System.out.println("Employee Present");
-	            System.out.println("Daily Wage = " + dailyWage);
+			int wage = PART_TIME_HOURS * WAGE_PER_HOUR;
 
-	        } else {
+			System.out.println("Part Time Employee");
+			System.out.println("Daily Wage = " + wage);
 
-	            System.out.println("Employee Absent");
-	            System.out.println("Daily Wage = 0");
-	        }
+		} else if (empCheck == 2) {
+
+			int wage = FULL_TIME_HOURS * WAGE_PER_HOUR;
+
+			System.out.println("Full Time Employee");
+			System.out.println("Daily Wage = " + wage);
+
+		} else {
+
+			System.out.println("Employee Absent");
+			System.out.println("Daily Wage = 0");
+		}
 	}
 
 }

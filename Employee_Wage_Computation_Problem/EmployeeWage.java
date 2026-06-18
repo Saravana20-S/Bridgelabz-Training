d3c85b316bb87de7d91b15e58372cd0be913c964
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-18 12:10:05 +0530

    refactor(EmployeeWage): UC2 Calculate Daily Employee Wage

--- Employee_Wage_Computation_Problem/EmployeeWage.java
+++ Employee_Wage_Computation_Problem/EmployeeWage.java
@@ -2,15 +2,26 @@ package com.bridgelabz.Employee_Wage_Computation_Problem;
 
 public class EmployeeWage {
 
+	static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOURS = 8;
+	
 	public static void main(String[] args) {
+		 
 		 System.out.println("Welcome to Employee Wage Computation Program");
 
 	        int attendance = (int) (Math.random() * 2);
 
 	        if (attendance == 1) {
-	            System.out.println("Employee is Present");
+
+	            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+
+	            System.out.println("Employee Present");
+	            System.out.println("Daily Wage = " + dailyWage);
+
 	        } else {
-	            System.out.println("Employee is Absent");
+
+	            System.out.println("Employee Absent");
+	            System.out.println("Daily Wage = 0");
 	        }
 	}
 

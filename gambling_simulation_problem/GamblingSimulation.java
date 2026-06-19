da792db9b19f7989b34071101d33a8b9ce541547
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-19 14:35:48 +0530

    feat(Gambling Simulation): Add Implement UC1 gambler stake and bet initialization

---
+++ gambling_simulation_problem/GamblingSimulation.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.gambling_simulation_problem;
+
+
+public class GamblingSimulation {
+	
+	public static void main(String[] args) {
+		int stake = 100;
+        int bet = 1;
+
+        System.out.println("Stake: $" + stake);
+        System.out.println("Bet per game: $" + bet);
+		
+	}
+
+}

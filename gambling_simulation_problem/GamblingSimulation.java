f7721fcc0ceff33008700118233256b29f89f1a8
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-19 14:39:41 +0530

    refactor(Gambling Simulation): Add Implement UC3 monthly gambling simulation for 20 days

--- gambling_simulation_problem/GamblingSimulation.java
+++ gambling_simulation_problem/GamblingSimulation.java
@@ -4,17 +4,23 @@ package com.bridgelabz.gambling_simulation_problem;
 public class GamblingSimulation {
 	
 	public static void main(String[] args) {
-		int stake = 100;
-        int bet = 1;
+		int totalAmount = 0;
 
-        while (stake > 50 && stake < 150) {
-            if (Math.random() < 0.5)
-                stake += bet;
-            else
-                stake -= bet;
+        for (int day = 1; day <= 20; day++) {
+
+            int stake = 100;
+
+            while (stake > 50 && stake < 150) {
+                if (Math.random() < 0.5)
+                    stake++;
+                else
+                    stake--;
+            }
+
+            totalAmount += (stake - 100);
         }
 
-        System.out.println("Final Stake: $" + stake);
+        System.out.println("Total Amount Won/Lost: $" + totalAmount);
 		
 	}
 

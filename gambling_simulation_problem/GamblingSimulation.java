18ec86d4bd0235bb6869ed9d26fbd9757fb1da0b
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-19 14:49:55 +0530

    refactor(Gambling Simulation): Add Implement UC6 decide whether to continue gambling

--- gambling_simulation_problem/GamblingSimulation.java
+++ gambling_simulation_problem/GamblingSimulation.java
@@ -41,6 +41,34 @@ public class GamblingSimulation {
 
 		System.out.println("Unluckiest Day: " + unluckiestDay +
 				" Lost $" + Math.abs(maxLoss));
+		
+		
+		
+		 int totalAmount = 0;
+
+	        for (int day = 1; day <= 20; day++) {
+
+	            int stake = 100;
+
+	            while (stake > 50 && stake < 150) {
+
+	                if (Math.random() < 0.5) {
+	                    stake++;
+	                } else {
+	                    stake--;
+	                }
+	            }
+
+	            totalAmount += (stake - 100);
+	        }
+
+	        System.out.println("Monthly Result: $" + totalAmount);
+
+	        if (totalAmount > 0) {
+	            System.out.println("Continue Gambling Next Month");
+	        } else {
+	            System.out.println("Stop Gambling");
+	        }
+	    }
 	}
 
-}

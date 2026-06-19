63ad6b356fd88fd9c75894ba4fdab40f81d9d43c
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-19 14:46:06 +0530

    refactor(Gambling Simulation): Add: Implement UC4 track winning and losing days

--- gambling_simulation_problem/GamblingSimulation.java
+++ gambling_simulation_problem/GamblingSimulation.java
@@ -4,24 +4,31 @@ package com.bridgelabz.gambling_simulation_problem;
 public class GamblingSimulation {
 	
 	public static void main(String[] args) {
-		int totalAmount = 0;
+		 int winningDays = 0;
+	        int losingDays = 0;
 
-        for (int day = 1; day <= 20; day++) {
+	        for (int day = 1; day <= 20; day++) {
 
-            int stake = 100;
+	            int stake = 100;
 
-            while (stake > 50 && stake < 150) {
-                if (Math.random() < 0.5)
-                    stake++;
-                else
-                    stake--;
-            }
+	            while (stake > 50 && stake < 150) {
 
-            totalAmount += (stake - 100);
-        }
+	                if (Math.random() < 0.5) {
+	                    stake++;
+	                } else {
+	                    stake--;
+	                }
+	            }
 
-        System.out.println("Total Amount Won/Lost: $" + totalAmount);
-		
-	}
+	            if (stake == 150) {
+	                winningDays++;
+	            } else {
+	                losingDays++;
+	            }
+	        }
+
+	        System.out.println("Winning Days: " + winningDays);
+	        System.out.println("Losing Days: " + losingDays);
+	    }
 
 }
